2d3aa7db27705b04c6c4985b878685c08d71533a
KhaledAshrafM <khaledashrafzzz583@gmail.com>
2024-05-16 15:21:39 +0300

    EDA

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,11 +5,9 @@ df = pd.read_csv('data/glassdoor_jobs.csv')
 
 df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
 df['employer'] = df['Salary Estimate'].apply(lambda x: 1 if "employer provided salary" in x.lower() else 0)
-
 df = df[df['Salary Estimate'] != '-1']
 salary = df['Salary Estimate'].apply(lambda x: x.split("(")[0])
 minus_kd = salary.apply(lambda x: x.replace("K", "").replace("$", ""))
-
 minus_hr = minus_kd.apply(lambda x: x.lower().replace("per hour", "").replace("employer provided salary:", ""))
 
 df["min_salary"], df["max_salary"] = minus_hr.apply(lambda x: int(x.split("-")[0])), minus_hr.apply(lambda x: int(x.split("-")[1]))
